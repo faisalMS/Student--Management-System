13b61ff9f7c0ed4950ec0d6fbb36213948fff4c3
FaisalMS <fis445@hotmail.com>
2024-01-03 00:12:01 +0300

    V0.1

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String students[] = {"Faisal", "Ahmed", "Marwan", "Ali"};
+        int grades[] = {85, 97, 70, 60};
+        
+        
+    }
+
+    public static void newStudentAndDegree(){
+        
+    }
+
+    public static void viewAddedStudents(){
+
+    }
+
+    public static void averageGradesOfStudents(){
+        
+    }
+}
